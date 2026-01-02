29995116bcff5291fd451f4ac9ac31f3666f978d
Eihr404 <israhernandezr@gmail.com>
2026-01-02 12:45:28 -0500

    Cambio en la estructura

--- Administracion/GUI/MainWindow.xaml.cs
+++ Administracion/GUI/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Administracion
+namespace Administracion.GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
